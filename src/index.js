6531ea9720486d90251537edd0e80009635590da
OlgaMilemis <89983348+OlgaMilemis@users.noreply.github.com>
2021-11-30 19:10:34 +0100

    Improved CSS and added button and hoover

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-
+function dayAndNight() {
+  let currentDay = new Date();
+  let dayNight = currentDay.getHours();
+  if (dayNight < 12) {
+    let bodyColor = document.querySelector("body");
+    bodyColor.style.background = "purple";
+  } else {
+    let bodyColor = document.querySelector("body");
+    bodyColor.style.background = "orange";
+  }
+}
+dayAndNight();
