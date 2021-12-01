f707c010ea5cc2c2cb5a9831637d87bcdef36734
OlgaMilemis <89983348+OlgaMilemis@users.noreply.github.com>
2021-12-01 12:03:28 +0100

    JS TemperatureFunction added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let cityElement = document.querySelector("#currentCity");
+  let temperatureElement = document.querySelector("#actualTemp");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#windSpeed");
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "5eac19cf21f53d5d30820a9a9bafd9f0";
+let units = "metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(displayTemperature);
+
 function dayAndNight() {
   let currentDay = new Date();
   let dayNight = currentDay.getHours();
