42c19ad3d2a782f56751ec7a844e6348c5e06484
OlgaMilemis <89983348+OlgaMilemis@users.noreply.github.com>
2021-12-03 11:03:34 +0100

    added Min and Max Temp

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ function displayTemperature(response) {
   console.log(response.data);
   let cityElement = document.querySelector("#currentCity");
   let temperatureElement = document.querySelector("#actualTemp");
+  let minTempElement = document.querySelector("#minTemp");
+  let maxTempElement = document.querySelector("#maxTemp");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#windSpeed");
   cityElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  minTempElement.innerHTML = Math.round(response.data.main.temp_min);
+  maxTempElement.innerHTML = Math.round(response.data.main.temp_max);
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
