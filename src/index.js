a523e4c0b16a2fc459b1b193e350b01112813aec
OlgaMilemis <89983348+OlgaMilemis@users.noreply.github.com>
2021-12-04 23:51:43 +0100

    Fahrenheit conversion added

--- src/index.js
+++ src/index.js
@@ -37,9 +37,12 @@ function showTemperature(response) {
 
   cityElement.innerHTML = response.data.name;
   dateDisplay.innerHTML = formatDate(currentTime);
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  minTempElement.innerHTML = Math.round(response.data.main.temp_min);
-  maxTempElement.innerHTML = Math.round(response.data.main.temp_max);
+  celsiusTemp = response.data.main.temp;
+  minCelsius = response.data.main.temp_min;
+  maxCelsius = response.data.main.temp_max;
+  temperatureElement.innerHTML = Math.round(celsiusTemp);
+  minTempElement.innerHTML = Math.round(minCelsius);
+  maxTempElement.innerHTML = Math.round(maxCelsius);
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
@@ -49,6 +52,7 @@ function showTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+
 function search(city) {
   let apiKey = "5eac19cf21f53d5d30820a9a9bafd9f0";
   let units = "metric";
@@ -62,7 +66,44 @@ function showCity(event) {
   search(cityInputElement.value);
 }
 
-search("Moscow");
+function showFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#actualTemp");
+  let minTempElement = document.querySelector("#minTemp");
+  let maxTempElement = document.querySelector("#maxTemp");
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  let minFahrenheit = (minCelsius * 9) / 5 + 32;
+  let maxFahrenheit = (maxCelsius * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+  minTempElement.innerHTML = Math.round(minFahrenheit);
+  maxTempElement.innerHTML = Math.round(maxFahrenheit);
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+}
+
+function showCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#actualTemp");
+  let minTempElement = document.querySelector("#minTemp");
+  let maxTempElement = document.querySelector("#maxTemp");
+  temperatureElement.innerHTML = Math.round(celsiusTemp);
+  minTempElement.innerHTML = Math.round(minCelsius);
+  maxTempElement.innerHTML = Math.round(maxCelsius);
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+}
+
+let celsiusTemp = null;
+let minCelsius = null;
+let maxCelsius = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", showCity);
+
+let fahrenheitLink = document.querySelector("#linkF");
+fahrenheitLink.addEventListener("click", showFahrenheit);
+
+let celsiusLink = document.querySelector("#linkC");
+celsiusLink.addEventListener("click", showCelsius);
+
+search("Moscow");
